093f1690f35fb89ff345679b294be7bdf617ed35
chohyunji1007 <10guswl7@gmail.com>
2024-01-14 20:16:07 +0900

    chapter 7 end

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,7 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    const data = await Promise.all([
+    const data = await Promise.all([ //데이터 병렬 처리! 모든 데이터를 한꺼번에 가져옴
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
